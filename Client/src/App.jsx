eb310d9b85a58b4df9f1e22e44007f56b1e90330
Othniel <chukchi2345@gmail.com>
2025-04-13 12:10:38 +0100

    Fixed Errors

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import SplashScreen from './dark_splash_screen.jsx'
+import SplashScreen from "./dark_splash_screen.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
-  const time = new Date();
-  const time_dics = {
-         hour: time.getHours(),
-         sec: time.getSeconds(),
-         min: time.getMinutes()
-     }
+  // const time = new Date();
+  // const time_dics = {
+  //   hour: time.getHours(),
+  //   sec: time.getSeconds(),
+  //   min: time.getMinutes(),
+  // };
   return (
-    <div>{"i am comming"}
-      <SplashScreen /> 
-     </div>
-    );
-  
+    <div>
+      {/* {"i am comming"} */}
+      <SplashScreen />
+    </div>
+  );
 }
 
 export default App;
--- Client/src/dark_splash_screen.jsx
+++ Client/src/dark_splash_screen.jsx
@@ -1,36 +1,38 @@
 import { useState } from "react";
 
 function SplashScreen(props) {
+  let [isloading, setIsLoading] = useState(false);
 
-    let [isloading, setIsLoading] = useState(true);
-    
-    const time = new Date();
-    const time_dic = {
-         hour: time.getHours(),
-         sec: time.getSeconds(),
-         min: time.getMinutes()
-     }
-     
-     const HandleLoadingComplete = () => {
-            setTimeout(()=>{ setIsLoading(false)},5000);
-              
-            let np =   isloading==false ? (<div> {time_dic.hour}:{time_dic.min}:{time_dic.sec}</div>): "";
-            [isloading, setIsLoading]= useState(true)
-            
-            return np
-    }
-    console.log(isloading);
-    return (
-        <div className="splash-screen">
-            < HandleLoadingComplete />
-            
+  const time = new Date();
+  const time_dic = {
+    hour: time.getHours(),
+    sec: time.getSeconds(),
+    min: time.getMinutes(),
+  };
+
+  const HandleLoadingComplete = () => {
+    setTimeout(() => {
+        setIsLoading(true);
+    }, 5000);
+
+    let np =
+      isloading == true ? (
+        <div>
+          {time_dic.hour}:{time_dic.min}:{time_dic.sec}
         </div>
-    );
+      ) : (
+        ""
+      );
+    // [isloading, setIsLoading] = useState(true);
 
-
-
-
-    
+    return np;
+  };
+  console.log(isloading);
+  return (
+    <div className="splash-screen">
+      <HandleLoadingComplete />
+    </div>
+  );
 }
 
 export default SplashScreen;
